14048f6446b582b8991d3dda989b32bd52ad8dc0
Geovana <geovanapederneschi@gmail.com>
2023-11-22 10:52:26 -0300

    Create APIFindRevisionWorkOrderByIdWorkOrderProcedure.php

---
+++ api/APIFindRevisionWorkOrderByIdWorkOrderProcedure.php
@@ -0,0 +1,60 @@
+<?php
+
+header('Content-type: application/json');
+
+ini_set('default_charset', 'utf-8');
+
+$response = array();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['app'] = "Mechanic") {
+
+    require_once('dbConnect.php');
+
+    mysqli_set_charset($con, "utf8");
+
+    $idOrdemProcedimento = $_POST["id"];
+
+    $statement = mysqli_prepare($con, 
+    "SELECT * FROM tb_revisao_ordem_funcionario WHERE id_tb_ordem_procedimento_enum_ordens = ?");
+    mysqli_stmt_bind_param($statement,"i",$idOrdemProcedimento);
+
+    mysqli_stmt_execute($statement);
+
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement,
+						    $idRevisaoOrdemFuncionario,
+						    $idOrdemFuncionario,
+                            $idProcedimentoOrdem,
+						    $cadastroDatetime
+                        ); 
+ 
+
+    if (mysqli_stmt_num_rows($statement) > 0) {
+
+        while (mysqli_stmt_fetch($statement)) {
+
+            array_push($response, array(
+                "idtb_revisao_ordem_funcionario" => $idRevisaoOrdemFuncionario,
+				"id_tb_r_ordem_procedimento_ordens_funcionario" => $idOrdemFuncionario,
+                "id_tb_ordem_procedimento_enum_ordens" => $idProcedimentoOrdem,
+				"datetime_cadastro" => $cadastroDatetime
+                )
+            );
+        }
+    }else{
+           $response["sucesso"] = false;
+           $response["error"] = "No queries";
+    }
+
+    echo json_encode($response);
+    
+} else {
+
+    
+    $response["sucesso"] = false;
+    $response["erro"] = "Method Error or App Denied";
+    
+    echo json_encode($response);
+}
+?>
+	
