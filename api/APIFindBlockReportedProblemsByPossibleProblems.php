5319a022e3b1b8e235a0a832ab0c55d7ba8a18b3
Geovana <geovanapederneschi@gmail.com>
2024-01-10 13:39:02 -0300

    Create StepDiagnostic and BlockReportedProblems

---
+++ api/APIFindBlockReportedProblemsByPossibleProblems.php
@@ -0,0 +1,79 @@
+<?php
+
+header('Content-type: application/json');
+
+ini_set('default_charset', 'utf-8');
+
+$response = array();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['app'] = "Mechanic") {
+
+    require_once('dbConnect.php');
+
+    mysqli_set_charset($con, "utf8");
+
+    $stringIDs = $_POST["IDs"];
+    $problemasIDs = explode(";",$stringIDs);
+
+    $tipos = "i";
+    $in = "?";
+    for ($i=1; $i < count($problemasIDs); $i++) { 
+        $tipos .= "i";
+        $in .= ", ?";
+    }
+
+    $string = "SELECT idtb_r_enum_bloco_tb_problemas_relatados,
+    id_tb_enum_bloco_problemas_relatados,
+    id_tb_enum_problemas_relatados
+    FROM tb_r_enum_bloco_tb_problemas_relatados
+    WHERE id_tb_enum_bloco_problemas_relatados IN (
+    SELECT id_tb_enum_bloco_problemas_relatados
+    FROM tb_r_enum_bloco_tb_problemas_relatados
+    WHERE id_tb_enum_problemas_relatados IN ($in)
+    GROUP BY id_tb_enum_bloco_problemas_relatados
+    HAVING COUNT(DISTINCT id_tb_enum_problemas_relatados) = (SELECT COUNT(DISTINCT id_tb_enum_problemas_relatados) FROM tb_r_enum_bloco_tb_problemas_relatados WHERE id_tb_enum_problemas_relatados IN ($in))
+    );";
+
+    $statement = mysqli_prepare($con, $string);
+
+    mysqli_stmt_bind_param($statement,$tipos,...$problemasIDs);
+
+    mysqli_stmt_execute($statement);
+
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement,
+						    $id,
+                            $idBloco,
+                            $idProblema
+                        ); 
+ 
+
+    if (mysqli_stmt_num_rows($statement) > 0) {
+
+        while (mysqli_stmt_fetch($statement)) {
+
+            array_push($response, array(
+                    "idtb_r_enum_bloco_tb_problemas_relatados" => $id,
+                    "id_tb_enum_bloco_problemas_relatados" => $idBloco,
+                    "id_tb_enum_problemas_relatados" => $idProblema
+                )
+            );
+        }
+    }else{
+           $response["sucesso"] = false;
+           $response["error"] = "No queries";
+    }
+
+    echo json_encode($response);
+    
+} else {
+
+    
+    $response["sucesso"] = false;
+    $response["erro"] = "Method Error or App Denied";
+    
+    echo json_encode($response);
+}
+?>
+	
+	
