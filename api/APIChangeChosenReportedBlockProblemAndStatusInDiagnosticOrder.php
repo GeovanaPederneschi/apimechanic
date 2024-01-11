86109ceffe510c6a5eb1190e9da27b585bca81db
Geovana <geovanapederneschi@gmail.com>
2024-01-11 10:08:26 -0300

    Update APIChangeChosenReportedBlockProblemAndStatusInDiagnosticOrder.php

--- api/APIChangeChosenReportedBlockProblemAndStatusInDiagnosticOrder.php
+++ api/APIChangeChosenReportedBlockProblemAndStatusInDiagnosticOrder.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['app'] = "Mechanic") {
 
     mysqli_set_charset($con, "utf8");
 
-    $id = $_POST["id"];
+    $idOrdemFuncionario = $_POST["id_ordem_funcionario"];
     $status = $_POST["status"];
     $idBloco = $_POST["id_bloco"];
 
@@ -21,10 +21,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['app'] = "Mechanic") {
     SET
     `id_bloco_problema_relatado_escolido` = ?,
     `status_ordem_diagnostico` = ?
-    WHERE `idtb_ordem_diagnostico` = ?
+    WHERE `idtb_r_ordem_procedimento_ordens_funcionario` = ?
     ");
     
-    mysqli_stmt_bind_param($statement,"isi",$idBloco, $status,$id);
+    mysqli_stmt_bind_param($statement,"isi",$idBloco, $status,$idOrdemFuncionario);
 
     mysqli_stmt_execute($statement);
   
