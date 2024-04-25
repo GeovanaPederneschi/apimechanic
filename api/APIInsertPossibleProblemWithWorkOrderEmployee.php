84a55421ba3774afb4665c490196499150f90014
Geovana <geovanapederneschi@gmail.com>
2024-04-25 08:47:47 -0300

    Update APIInsertPossibleProblemWithWorkOrderEmployee.php

--- api/APIInsertPossibleProblemWithWorkOrderEmployee.php
+++ api/APIInsertPossibleProblemWithWorkOrderEmployee.php
@@ -13,14 +13,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['app'] = "Mechanic") {
     mysqli_set_charset($con, "utf8");
 
     $idProblema = $_POST["idProblema"];
-    $idFuncionario = $_POST["id_ordem_funcionario"];
+    $cpfFuncionario = $_POST["cpf_funcionario"];
     $status = $_POST["status"];
 
     $statement = mysqli_prepare($con, 
-    "INSERT INTO `auto_mechanic`.`tb_r_problema_funcionario_ordem`(`id_enum_problema_relatado`,`id_ordens_funcionario`,`status`)
-    VALUES ( ?, ?, ?)");
+    "INSERT INTO `auto_mechanic`.`tb_r_problema_funcionario_ordem`(`id_enum_problema_relatado`,`cpf_funcionario`,`status`)VALUES ( ?, ?, ?)");
 
-    mysqli_stmt_bind_param($statement,'iis',$idProblema,$idFuncionario,$status);
+    mysqli_stmt_bind_param($statement,'iss',$idProblema,$cpfFuncionario,$status);
 
     mysqli_stmt_execute($statement);
   
