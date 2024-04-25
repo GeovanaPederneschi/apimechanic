7f2605f985eaf7f5e21d04f291a76d8246e6b752
Geovana <geovanapederneschi@gmail.com>
2024-04-25 11:21:55 -0300

    Create APIFindAllLikeOrder.php

---
+++ APIFindAllLikeOrder.php
@@ -0,0 +1,72 @@
+<?php
+
+header('Content-type: application/json');
+
+ini_set('default_charset', 'utf-8');
+
+$response = array();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['app'] = "Mechanic") {
+
+    require_once('dbConnect.php');
+
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+    mysqli_set_charset($con, "utf8");
+
+    $ordemLike = "%{$_POST['ordem']}%";
+    $ordem = $_POST['ordem'];
+
+    $statement = mysqli_prepare($con, 
+    "SELECT * FROM tb_enum_ordens");
+
+    mysqli_stmt_execute($statement);
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement,
+						    $id,
+                            $codTSV,
+                            $tipoOperacao,
+						    $nome,
+                            $tempo,
+                            $modoAtribuicao,
+                            $tipoOrdem,
+                            $tipoSeccao
+                        ); 
+ 
+
+    if (mysqli_stmt_num_rows($statement) > 0) {
+
+        while (mysqli_stmt_fetch($statement)) {
+
+            array_push($response, array(
+                "id_ordens" => $id,
+                "cod_tsv" => $codTSV,
+                "tipo_operacao" => $tipoOperacao,
+				"nome_ordens" => $nome,
+                "tempo_padrao" => $tempo,
+                "modo_atribuicao" => $modoAtribuicao,
+                "categoria_ordem" => $tipoOrdem,
+                "categoria_seccao" => $tipoSeccao
+                )
+            );
+        }
+        
+          
+          
+    } else {
+    
+           $response["sucesso"] = false;
+           $response["error"] = "no queries";
+    }
+
+    echo json_encode($response);
+    
+} else {
+
+    
+    $response["sucesso"] = false;
+    
+    echo json_encode($response);
+}
+?>
+	
