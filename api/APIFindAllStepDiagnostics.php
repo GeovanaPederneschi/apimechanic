fd0acdac35b39eefcfbda6eab853d7dcad82bd29
Geovana <geovanapederneschi@gmail.com>
2024-05-02 13:36:13 -0300

    find stepDiagnostic

---
+++ api/APIFindAllStepDiagnostics.php
@@ -0,0 +1,61 @@
+<?php
+
+header('Content-type: application/json');
+
+ini_set('default_charset', 'utf-8');
+
+$response = array();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['app'] = "Mechanic") {
+
+    require_once('dbConnect.php');
+
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+    mysqli_set_charset($con, "utf8");
+
+    $statement = mysqli_prepare($con, 
+    "SELECT * FROM tb_enum_passos_diagnostico");
+
+    mysqli_stmt_execute($statement);
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement,
+                            $idPasso,
+                            $passo,
+                            $categoriaPasso,
+                            $tipoInsercao
+                        ); 
+ 
+
+    if (mysqli_stmt_num_rows($statement) > 0) {
+
+        while (mysqli_stmt_fetch($statement)) {
+
+            array_push($response, array(
+                "idtb_passos_diagnostico" => $idPasso,
+				"nome_passo" => $passo,
+                "categoria_passos_diagnostico" => $categoriaPasso,
+                "tipo_insercao" => $tipoInsercao
+                )
+            );
+        }
+        
+          
+          
+    } else {
+    
+           $response["sucesso"] = false;
+           $response["error"] = "no queries";
+    }
+
+    echo json_encode($response);
+    
+} else {
+
+    
+    $response["sucesso"] = false;
+    
+    echo json_encode($response);
+}
+?>
+	
