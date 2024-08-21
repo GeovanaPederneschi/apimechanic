7749b312bc0b70996be1d1d7d3c35f36e058e3a5
Geovana <geovanapederneschi@gmail.com>
2024-08-21 20:13:22 -0300

    ala

---
+++ api/APITeste.php
@@ -0,0 +1,24 @@
+<?php
+
+header('Content-type: application/json');
+
+ini_set('default_charset', 'utf-8');
+
+$response = array();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+           $response["sucesso"] = true;
+           $response["ala"] = "ala";
+
+    echo json_encode($response);
+    
+} else {
+
+    
+    $response["sucesso"] = false;
+    
+    echo json_encode($response);
+}
+?>
+	
