ba2edce4f39d9e82160318a9cb62976ca5286298
Geovana <geovanapederneschi@gmail.com>
2024-08-21 20:42:58 -0300

    ala

--- api/APITeste.php
+++ api/APITeste.php
@@ -6,7 +6,7 @@ ini_set('default_charset', 'utf-8');
 
 $response = array();
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
            $response["sucesso"] = true;
            $response["ala"] = "ala";
