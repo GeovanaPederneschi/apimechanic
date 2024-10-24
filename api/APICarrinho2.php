b3599ccffc7051baef6d8f9fd8c3c29f1b3766ac
Geovana Pederneschi <geovanapederneschi@gmail.com>
2024-10-24 20:38:46 -0300

    Atualização

--- api/APICarrinho2.php
+++ api/APICarrinho2.php
@@ -14,8 +14,8 @@ $debugMessages = array();
 // Função para converter RSSI para distância
 function rssiToDistance($rssi, $rssiRef, $n) {
     global $debugMessages;
-    $distance =  pow(10, ($rssiRef - $rssi) / 10*$n);
-    //return $rssi/$rssiRef;
+    //$distance =  pow(10, ($rssiRef - $rssi) / 10*$n);
+    $distance =  $rssi/$rssiRef;
     $debugMessages[] = "RSSI: $rssi -> Distância calculada: $distance metros"; // Armazena a mensagem de depuração
     return $distance;
 }
