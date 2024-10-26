d7f8af5473d391d8d59937b08a493d098089a7d6
Geovana Pederneschi <geovanapederneschi@gmail.com>
2024-10-26 09:36:48 -0300

    Atualização10

--- api/FindByLikeName.php
+++ api/FindByLikeName.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['app'] = "Mechanic") {
 
             array_push($response, array(
                 "id" => $id,
-				"nome_peca" => $dataCadastro,
+				"nome_peca" => $nomePeca,
 				"descricao" => $tipoCliente
                 )
             );
